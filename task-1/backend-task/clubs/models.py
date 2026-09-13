e5bce57065a081f07fee9c15080246db9fca7809
Nairit Shah <nairitshah.dev@gmail.com>
2026-09-13 17:11:39 +0530

    Add club management models
    Club, Member, Event, and update .gitignore

--- task-1/backend-task/clubs/models.py
+++ task-1/backend-task/clubs/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Club(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    motto = models.CharField(max_length = 100)
+    category = models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+class Member(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    phone = models.CharField(max_length = 10)
+    joined_at = models.DateTimeField(auto_now_add = True)
+    clubs = models.ManyToManyField(Club)
+
+class Event(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length = 100)
+    club = models.ForeignKey(Club, on_delete = models.CASCADE)
+    registrants = models.ManyToManyField(Member)
